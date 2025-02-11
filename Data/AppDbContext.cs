bc15a4272457b1069e3ae8abd00324c4102dcc3f
Subhrojit Hore <subhrojithore35@gmail.com>
2025-02-11 21:23:07 +0530

    validations added

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,12 +7,4 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<Employee> Employees { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseSqlServer("Server=localhost;Database=EmployeesDB;User Id=sa;Password=your_password;");
-        }
-    }
 }
